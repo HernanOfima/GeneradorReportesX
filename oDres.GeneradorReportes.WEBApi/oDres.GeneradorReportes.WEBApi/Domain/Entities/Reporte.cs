148808804ad27e03cec393e25b15e9d5f6f0d694
ARBOLEDAMESA\herna <hernan.arboleda.vergara@gmail.com>
2026-03-13 09:36:40 -0500

    Ajustes Orden Mostrar , Filtros en Grid , Formato segun tipo dato

--- oDres.GeneradorReportes.WEBApi/oDres.GeneradorReportes.WEBApi/Domain/Entities/Reporte.cs
+++ oDres.GeneradorReportes.WEBApi/oDres.GeneradorReportes.WEBApi/Domain/Entities/Reporte.cs
@@ -5,6 +5,7 @@ public class Reporte
     public Guid IdReporte { get; set; }
     public string? Nombre { get; set; }
     public string? Titulo { get; set; }
+    public int? OrdenMostrar { get; set; }
     public Guid IdModulo { get; set; }
     public string? SentenciaSQL { get; set; }
     public int TipoReporte { get; set; }
